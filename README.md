9cb04a7eba2dea31ce0655b519b384720b5b806f
Jeff <84156730+JeffM-Code@users.noreply.github.com>
2024-07-20 14:23:27 +0100

    new project app

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# TurbinePowerConsumptionSimulationModel
-ML model application
+## Power Consumption Simulation Model<br><br>
+[Model development](https://github.com/JeffM-Code/PortfolioWork/tree/main/ML/GasTurbineConsumption)<br><br>
+App<br><br>
+
+The slider changes the % of data applied onto model for it to predict on, and the graphs either show voltage input over time or actual and predicted power over time.<br><br>
+
+### Example:<br><br>
+<img src="example.png" alt="example" width="650"/><br><br>
+
+### Model analysis:<br><br>
+```
+X_train = train_df[['input_voltage']]
+y_train = train_df['el_power']
+
+X_test = test_df[['input_voltage']]
+y_test = test_df['el_power']
+
+```
+<br>
+
+Input voltage and electrical power features directly influence model performance, as the better the selction of training features or predictors, the better the quality of predictions output by the model.<br><br>
+
+The input voltage is an important factor that influences the electrical power output of the micro gas turbine, with the relationship between input voltage and electrical power being dynamic and time-dependent, and as a ket factor, input voltage should then be a significant predictor of electrical power (el_power), therefore is a good feature to select for the model.<br><br>
+
+
+### Expected Model Performance:<br><br>
+
+```
+Mean Squared Error: 236800.64012962865
+R^2 Score: 0.6477735114045802
+
+```
---
+++ streamlit_app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import time
+
+test_df = pd.read_csv('test.csv')
+
+with open('linear_regression_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+st.title("Power Consumption Simulation Model")
+
+test_size = st.slider("Select % of test data to showcase:", 10, 100, 10) / 100.0
+
+num_samples = int(len(test_df) * test_size)
+test_df_sampled = test_df.iloc[:num_samples]
+
+voltage_placeholder = st.empty()
+predict_placeholder = st.empty()
+run_turbine_placeholder = st.empty()
+
+def plot_voltage_over_time(data):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(data['time'], data['input_voltage'], color='blue', label='Input Voltage')
+    ax.set_xlabel('Time [sec]')
+    ax.set_ylabel('Input Voltage [V]')
+    ax.set_title('Input Voltage over Time')
+    ax.legend()
+    ax.grid(True)
+    return fig
+
+def plot_power_over_time(data, y_test, y_pred, title='Actual vs. Predicted Electrical Power over Time'):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(data['time'], y_test, color='green', label='Actual Electrical Power')
+    ax.plot(data['time'], y_pred, color='red', linestyle='--', label='Predicted Electrical Power')
+    ax.set_xlabel('Time [sec]')
+    ax.set_ylabel('Electrical Power [W]')
+    ax.set_title(title)
+    ax.legend()
+    ax.grid(True)
+    return fig
+
+voltage_placeholder.pyplot(plot_voltage_over_time(test_df_sampled))
+
+if st.button('Predict'):
+    X_test = test_df_sampled[['input_voltage']]
+    y_test = test_df_sampled['el_power']
+    y_pred = model.predict(X_test)
+
+    predict_placeholder.pyplot(plot_power_over_time(test_df_sampled, y_test, y_pred))
+
+if st.button('Simulate Turbine'):
+    for i in range(1, 11):
+        st.write(f"Displaying {i*10}% of the data")
+
+        num_samples = int(len(test_df) * (i*10) / 100.0)
+        test_df_sampled = test_df.iloc[:num_samples]
+
+        X_test = test_df_sampled[['input_voltage']]
+        y_test = test_df_sampled['el_power']
+        y_pred = model.predict(X_test)
+
+        voltage_placeholder.pyplot(plot_voltage_over_time(test_df_sampled))
+        run_turbine_placeholder.pyplot(plot_power_over_time(test_df_sampled, y_test, y_pred, title=f'Run Turbine: {i*10}% Data'))
+
+        # Simulate behavior of turbine running
+        time.sleep(0.4)
